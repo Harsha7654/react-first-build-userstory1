be96f0bd8bee764240b7633c1756f9c86a39ec0e
Harsha7654 <161643598+Harsha7654@users.noreply.github.com>
2024-11-18 23:57:00 +0000

    Half completed with UI implementation
    The video is completed till 2 hours on the dot

--- src/components/entities/subjects/SubjectForm.js
+++ src/components/entities/subjects/SubjectForm.js
@@ -1,13 +1,113 @@
+import { useState } from "react";
+import { Tray, Submit, Cancel } from "../../UI/Actions";
+import FormItem from "../../UI/Form";
+
 const emptySubject = {
-  SubjectName: "",
-  SubjectImageURL: "",
+  SubjectName: "Social Studies",
+  SubjectImageURL: "socials_image.png",
   SubjectLecturerID: 0,
 };
 
-export default function SubjectForm({ initialsubject = emptySubject }) {
+export default function SubjectForm({
+  onDismiss,
+  initialsubject = emptySubject,
+}) {
   // Initialisation ------------------------------
+  const isValid = {
+    SubjectName: (name) => name.length > 8,
+    SubjectImageURL: (img) => img.length > 10,
+    SubjectLecturerID: (id) => id > 0 && id < 4,
+  };
+
+  const errorMessage = {
+    SubjectName: "Subject Name is too short",
+    SubjectImageURL: "Image URL is not valid",
+    SubjectLecturerID: "Invalid ID",
+  };
   // State ---------------------------------------
+  const [subject, setSubject] = useState(initialsubject);
+  const [errors, setErrors] = useState(
+    Object.keys(initialsubject).reduce(
+      (accum, key) => ({ ...accum, [key]: null }),
+      {}
+    )
+  );
   // Handlers ------------------------------------
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    const newValue = name === "SubjectLecturerID" ? parseInt(value) : value;
+    setSubject({ ...subject, [name]: newValue });
+    setErrors({
+      ...errors,
+      [name]: isValid[name](newValue) ? null : errorMessage[name],
+    });
+  };
+
+  const handleCancel = () => onDismiss();
+  const handleSubmit = () => {};
   // View ----------------------------------------
-  return;
+  return (
+    <form className="BorderedForm">
+      <FormItem
+        label="Subject Name"
+        htmlFor="SubjectName"
+        advice="Please enter the name of the subject"
+        error={errors.SubjectName}
+      >
+        <input
+          type="text"
+          name="SubjectName"
+          value={subject.SubjectName}
+          onChange={handleChange}
+        />
+      </FormItem>
+
+      <FormItem
+        label="Suject Image URL"
+        htmlFor="SubjectImageURL"
+        advice="Please enter the picture URL of the subject"
+        error={errors.SubjectImageURL}
+      >
+        <input
+          type="text"
+          name="SubjectImageURL"
+          value={subject.SubjectImageURL}
+          onChange={handleChange}
+        />
+      </FormItem>
+
+      <FormItem
+        label="Suject Lecturer ID"
+        htmlFor="SubjectLecturerID"
+        advice="Choose the ID between 1 and 5 but choose 1"
+        error={errors.SubjectLecturerID}
+      >
+        <select
+          name="SubjectLecturerID"
+          value={subject.SubjectLecturerID}
+          onChange={handleChange}
+        >
+          <option value="0" disabled>
+            Select lecturer ID 1
+          </option>
+          {[1, 2, 3, 4, 5, 6].map((id) => (
+            <option key={id}>{id}</option>
+          ))}
+        </select>
+      </FormItem>
+
+      <Tray>
+        <Submit
+          showText
+          onClick={handleSubmit}
+          buttonText="Submit new subject"
+        />
+        <Cancel
+          showText
+          onClick={handleCancel}
+          buttonText="Cancel submission"
+        />
+      </Tray>
+    </form>
+  );
 }
